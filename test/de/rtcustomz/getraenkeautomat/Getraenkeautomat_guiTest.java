df445a291136f4b713780449ed8dfa0dcc02a849
Krathegon <fabian@familie-kalkofen.de>
2015-05-07 17:43:22 +0200

    - changed jdk to 1.7 - added required test directory - added simple test class

---
+++ test/de/rtcustomz/getraenkeautomat/Getraenkeautomat_guiTest.java
@@ -0,0 +1,16 @@
+package de.rtcustomz.getraenkeautomat;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class Getraenkeautomat_guiTest  extends GWTTestCase {
+
+	@Override
+	public String getModuleName() {
+		return "de.rtcustomz.getraenkeautomat.Getraenkeautomat_gui";
+	}
+	
+	public void myFirstTest() {
+		assertTrue(true);
+	}
+
+}
