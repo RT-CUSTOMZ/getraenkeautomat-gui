9dd8610f520b8343ad639e160872eb719a2b64d0
Krathegon <fabian@familie-kalkofen.de>
2015-06-13 00:44:45 +0200

    added google charts

--- src/main/java/de/rtcustomz/getraenkeautomat/client/GetraenkeautomatGUI.java
+++ src/main/java/de/rtcustomz/getraenkeautomat/client/GetraenkeautomatGUI.java
@@ -1,15 +1,24 @@
 package de.rtcustomz.getraenkeautomat.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.dom.client.HeadingElement;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.user.client.ui.SimpleLayoutPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.googlecode.gwt.charts.client.ChartLoader;
+import com.googlecode.gwt.charts.client.ChartPackage;
+import com.googlecode.gwt.charts.client.ColumnType;
+import com.googlecode.gwt.charts.client.DataTable;
+import com.googlecode.gwt.charts.client.corechart.PieChart;
 
 public class GetraenkeautomatGUI implements EntryPoint {
 	// private final Messages messages = GWT.create(Messages.class);
 
 	// private final ModelRequestFactory requestFactory = GWT.create(ModelRequestFactory.class);
+	
+	private SimpleLayoutPanel layoutPanel;
+    private PieChart pieChart;
 
 	/**
 	 * This is the entry point method.
@@ -33,8 +42,26 @@ public class GetraenkeautomatGUI implements EntryPoint {
     	FlowPanel content = new FlowPanel();
     	content.getElement().setId("content");
     	
+    	
+    	
+    	content.add(getSimpleLayoutPanel());
+
+        // Create the API Loader
+        ChartLoader chartLoader = new ChartLoader(ChartPackage.CORECHART);
+        chartLoader.loadApi(new Runnable() {
+
+            @Override
+            public void run() {
+                getSimpleLayoutPanel().setWidget(getPieChart());
+                drawPieChart();
+            }
+        });
+
+    	
+    	
+    	
     	// TODO: add content to contentFlowPanel, e.g.:
-    	content.add(new HTMLPanel(HeadingElement.TAG_H1, "Hier kommt der Content rein!!!"));
+    	// content.add(new HTMLPanel(HeadingElement.TAG_H1, "Hier kommt der Content rein!!!"));
     	
     	wrapper.add(content);
     	wrapper.add(footer);
@@ -75,4 +102,38 @@ public class GetraenkeautomatGUI implements EntryPoint {
 //			}
 //		});
 	}
+	
+	private SimpleLayoutPanel getSimpleLayoutPanel() {
+        if (layoutPanel == null) {
+            layoutPanel = new SimpleLayoutPanel();
+        }
+        return layoutPanel;
+	}
+	
+	private Widget getPieChart() {
+		if (pieChart == null) {
+	        pieChart = new PieChart();
+		}
+		return pieChart;
+	}
+	
+	private void drawPieChart() {
+		// Prepare the data
+		DataTable dataTable = DataTable.create();
+		dataTable.addColumn(ColumnType.STRING, "Name");
+		dataTable.addColumn(ColumnType.NUMBER, "Donuts eaten");
+		dataTable.addRows(4);
+		dataTable.setValue(0, 0, "Michael");
+		dataTable.setValue(1, 0, "Elisa");
+		dataTable.setValue(2, 0, "Robert");
+		dataTable.setValue(3, 0, "John");
+		dataTable.setValue(0, 1, 5);
+		dataTable.setValue(1, 1, 7);
+		dataTable.setValue(2, 1, 3);
+		dataTable.setValue(3, 1, 2);
+		
+		// Draw the chart
+		pieChart.draw(dataTable);
+	}
+
 }
