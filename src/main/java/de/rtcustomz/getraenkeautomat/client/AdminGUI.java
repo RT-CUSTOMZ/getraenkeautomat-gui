2f995c8afb5a15794ed3c13dfd51047b5d4b06c7
Dennis Ahrens <dennis.ahrens@gmx.net>
2015-06-20 23:29:01 +0200

    we can now select between AdminCardPage and AdminUserPage from Navigation

--- src/main/java/de/rtcustomz/getraenkeautomat/client/AdminGUI.java
+++ src/main/java/de/rtcustomz/getraenkeautomat/client/AdminGUI.java
@@ -1,73 +1,46 @@
 package de.rtcustomz.getraenkeautomat.client;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-
-import com.google.gwt.cell.client.EditTextCell;
-import com.google.gwt.cell.client.FieldUpdater;
-import com.google.gwt.cell.client.SelectionCell;
-import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.HeadingElement;
-import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.user.cellview.client.Column;
-import com.google.gwt.user.cellview.client.ColumnSortEvent.ListHandler;
-import com.google.gwt.user.cellview.client.DataGrid;
-import com.google.gwt.user.cellview.client.SimplePager;
-import com.google.gwt.user.cellview.client.SimplePager.TextLocation;
-import com.google.gwt.user.client.Window;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.view.client.DefaultSelectionEventManager;
-import com.google.gwt.view.client.ListDataProvider;
-import com.google.gwt.view.client.MultiSelectionModel;
-import com.google.gwt.view.client.SelectionModel;
-import com.google.web.bindery.event.shared.EventBus;
-import com.google.web.bindery.event.shared.SimpleEventBus;
-import com.google.web.bindery.requestfactory.shared.Receiver;
-import com.google.web.bindery.requestfactory.shared.ServerFailure;
-
-import de.rtcustomz.getraenkeautomat.client.proxies.CardProxy;
-import de.rtcustomz.getraenkeautomat.client.proxies.UserProxy;
-import de.rtcustomz.getraenkeautomat.shared.ModelRequestFactory;
-import de.rtcustomz.getraenkeautomat.shared.requests.CardRequest;
-import de.rtcustomz.getraenkeautomat.shared.requests.UserRequest;
 
 public class AdminGUI implements EntryPoint {
-	//private final Messages messages = GWT.create(Messages.class);
-
-	private final ModelRequestFactory requestFactory = GWT.create(ModelRequestFactory.class);
-	
-	//-----\/-----
-	DataGrid<CardProxy> dataGrid;
-	SimplePager pager;
-	
-    private final ListDataProvider<CardProxy> dataProvider = new ListDataProvider<CardProxy>();
-    //final ListDataProvider<UserProxy> userProvider = new ListDataProvider<UserProxy>();
-    List<UserProxy> userlist = new ArrayList<UserProxy>();
-    
-	CardRequest request_card = requestFactory.cardRequest();
-	CardProxy newCardProxy = request_card.create(CardProxy.class);
-	
-	UserRequest request_user = requestFactory.userRequest();
-	UserProxy newUserProxy = request_user.create(UserProxy.class);
-	
-	final EventBus eventBus = new SimpleEventBus();
-	
-	final HTML cardLoadLabel = new HTML();
-	final HTML userLoadLabel = new HTML();
-	final HTML debugLabel = new HTML();
-	//-----/\-----
-	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+        String initToken = History.getToken();
+        
+        if (initToken.length() == 0) {
+          History.newItem(AdminCardPage.getPageName());
+        }
+        
+        History.addValueChangeHandler(new ValueChangeHandler<String>() {
+			
+            @Override
+            public void onValueChange(ValueChangeEvent<String> event) {
+                System.out.println("Current State : " + event.getValue());
+                
+                if (event.getValue().equals(AdminCardPage.getPageName())){
+                    RootPanel.get().clear();
+                    showPage(AdminCardPage.getInstance());
+                }
+                
+                else if (event.getValue().equals(AdminUserPage.getPageName())){
+                    RootPanel.get().clear();
+                    showPage(AdminUserPage.getInstance());;
+                }    
+            }
+		});
+        History.fireCurrentHistoryState(); 
+	}
+	
+	private void showPage(Composite page)
+	{
 		MyResources.INSTANCE.css().ensureInjected();
 		
 		FlowPanel wrapper = new FlowPanel();
@@ -75,7 +48,7 @@ public class AdminGUI implements EntryPoint {
 		clear.setStyleName("clear");
 
 		Header header = new Header();
-		Navigation nav = new Navigation("Karten","Benutzer");
+		Navigation nav = new Navigation(AdminCardPage.getPageName(),AdminUserPage.getPageName());
 		Footer footer = new Footer();
 
 		wrapper.getElement().setId("wrapper");
@@ -88,220 +61,11 @@ public class AdminGUI implements EntryPoint {
     	
     	// TODO: add content to contentFlowPanel, e.g.:
     	//content.add(new HTMLPanel(HeadingElement.TAG_H1, "Hier kommt der Content rein!!!"));
-    	
-    	//-----\/-----
-    	requestFactory.initialize(eventBus);
-		dataGrid = new DataGrid<CardProxy>();//(KEY_PROVIDER);
-		dataGrid.setWidth("100%");
-		dataGrid.setHeight("300px");
-		
-		//dataGrid.setAutoHeaderRefreshDisabled(true);
-		
-		dataGrid.setEmptyTableWidget(new Label("Leere Tabelle..."));
-		final ListHandler<CardProxy> sortHandler = new ListHandler<CardProxy>(dataProvider.getList());
-		dataGrid.addColumnSortHandler(sortHandler);
-		
-		SimplePager.Resources pagerResources = GWT.create(SimplePager.Resources.class);
-		pager = new SimplePager(TextLocation.CENTER, pagerResources, false, 0, true);
-		pager.setDisplay(dataGrid);
-		
-		final SelectionModel<CardProxy> selectionModel = new MultiSelectionModel<CardProxy>();
-		dataGrid.setSelectionModel(selectionModel, DefaultSelectionEventManager.<CardProxy> createCheckboxManager());
-		
-		request_user.findAllUsers().fire(new Receiver<List<UserProxy>>() {
-			@Override
-			public void onSuccess(List<UserProxy> users) {
-				setUserList(users);
-				initTableColumns(selectionModel, sortHandler);
-				showGrid();
-				userLoadLabel.setHTML("<p>Status: User Data loaded</p>");
-			}
-			@Override
-			public void onFailure(ServerFailure error) {
-				userLoadLabel.setHTML("<p>"+error.getMessage()+"</p>");
-			}
-		});
-		
-		content.add(cardLoadLabel);
-		content.add(userLoadLabel);
-		content.add(debugLabel);
-		content.add(dataGrid);
-		content.add(pager);
-		
-    	//-----/\-----
+    	content.add(page);
     	
     	wrapper.add(content);
     	wrapper.add(footer);
     	
     	RootPanel.get().add(wrapper);
-	}
-	
-	/**
-	 * Add the columns to the table.
-	 */
-	private void initTableColumns(final SelectionModel<CardProxy> selectionModel,
-	    ListHandler<CardProxy> sortHandler) {
-//	  // Checkbox column. This table will uses a checkbox column for selection.
-//	  // Alternatively, you can call dataGrid.setSelectionEnabled(true) to enable
-//	  // mouse selection.
-//	  Column<CardProxy, Boolean> checkColumn =
-//	      new Column<CardProxy, Boolean>(new CheckboxCell(true, false)) {
-//	        @Override
-//	        public Boolean getValue(CardProxy object) {
-//	          // Get the value from the selection model.
-//	          return selectionModel.isSelected(object);
-//	        }
-//	      };
-//	  dataGrid.addColumn(checkColumn, SafeHtmlUtils.fromSafeConstant("<br/>"));
-//	  dataGrid.setColumnWidth(checkColumn, 40, Unit.PX);
-//		final List<CardProxy> card_list = new ArrayList<CardProxy>();
-		
-	  // id.
-	  Column<CardProxy, String> idColumn =
-	      new Column<CardProxy, String>(new TextCell()) {
-	        @Override
-	        public String getValue(CardProxy object) {
-	          return object.getId();
-	        }
-	      };
-	  idColumn.setSortable(true);
-	  sortHandler.setComparator(idColumn, new Comparator<CardProxy>() {
-	    @Override
-	    public int compare(CardProxy o1, CardProxy o2) {
-	      return o1.getId().compareTo(o2.getId());
-	    }
-	  });
-	  dataGrid.addColumn(idColumn, "ID");
-	  //dataGrid.setColumnWidth(idColumn, 140, Unit.PX);
-	  dataGrid.setColumnWidth(idColumn, 17, Unit.PCT);
-	
-	  // type.
-	  Column<CardProxy, String> typeNameColumn =
-	      new Column<CardProxy, String>(new TextCell()) {
-	        @Override
-	        public String getValue(CardProxy object) {
-	          return object.getType();
-	        }
-	      };
-	  typeNameColumn.setSortable(true);
-	  sortHandler.setComparator(typeNameColumn, new Comparator<CardProxy>() {
-	    @Override
-	    public int compare(CardProxy o1, CardProxy o2) {
-	      return o1.getType().compareTo(o2.getType());
-	    }
-	  });
-	  dataGrid.addColumn(typeNameColumn, "Type");
-	  //dataGrid.setColumnWidth(typeNameColumn, 140, Unit.PX);
-	  dataGrid.setColumnWidth(typeNameColumn, 17, Unit.PCT);
-		
-	  // Description.
-	  Column<CardProxy, String> descriptionColumn =
-	      new Column<CardProxy, String>(new EditTextCell()) {
-	        @Override
-	        public String getValue(CardProxy object) {
-	          return object.getDescription();
-	        }
-	      };
-	  descriptionColumn.setSortable(true);
-	  sortHandler.setComparator(descriptionColumn, new Comparator<CardProxy>() {
-	    @Override
-	    public int compare(CardProxy o1, CardProxy o2) {
-	      return o1.getDescription().compareTo(o2.getDescription());
-	    }
-	  });
-	  dataGrid.addColumn(descriptionColumn, "Description");
-	  descriptionColumn.setFieldUpdater(new FieldUpdater<CardProxy, String>() {
-	    @Override
-	    public void update(int index, CardProxy object, String value) {
-	      // Called when the user changes the value.
-	    	request_card = requestFactory.cardRequest();
-	    	CardProxy card = request_card.edit(object);
-	    	card.setDescription(value);
-	    	request_card.save(card).fire(new Receiver<Void>(){
-				@Override
-				public void onSuccess(Void arg0) {
-					dataProvider.refresh();
-				}
-	    		
-	    	});
-	    }
-	  });
-	  //dataGrid.setColumnWidth(descriptionColumn, 330, Unit.PX);
-	  dataGrid.setColumnWidth(descriptionColumn, 33, Unit.PCT);
-	  
-	  // user
-	    //final List<UserProxy> users = userProvider.getList();
-		//Window.alert("userlist size: "+userlist.size());
-	    List<String> userNames = new ArrayList<String>();
-	    userNames.add("-");
-	    for (UserProxy user : userlist) {
-	      //userNames.add(user.getFirstname()+" "+user.getLastname());
-	    	userNames.add(getUserDisplayName(user));
-	    }
-	    SelectionCell userCell = new SelectionCell(userNames);
-	    Column<CardProxy, String> userColumn = new Column<CardProxy, String>(userCell) {
-	      @Override
-	      public String getValue(CardProxy object) {
-	    	  UserProxy user = object.getUser();
-	    	  //if(user!=null) return user.getFirstname()+" "+user.getLastname();
-	    	  //else return "";
-	    	  return getUserDisplayName(user);
-	      }
-	    };
-	    dataGrid.addColumn(userColumn, "User");
-	    userColumn.setFieldUpdater(new FieldUpdater<CardProxy, String>() {
-	      @Override
-	      public void update(int index, CardProxy object, String value) {
-	        for (UserProxy user : userlist) {
-	        	//String name = user.getFirstname()+" "+user.getLastname();
-	        	String name = getUserDisplayName(user);
-	        	if (name.equals(value)) {
-	        		request_card = requestFactory.cardRequest();
-	        		CardProxy card = request_card.edit(object);
-			    	card.setUser(user);
-			    	request_card.save(card).with("user").fire(new Receiver<Void>(){
-						@Override
-						public void onSuccess(Void arg0) {
-							dataProvider.refresh();
-						}
-			    		
-			    	});
-			    }
-	        }
-	      }
-	    });
-	    //dataGrid.setColumnWidth(userColumn, 310, Unit.PX);
-	    dataGrid.setColumnWidth(userColumn, 33, Unit.PCT);
-	}
-	
-	private void showGrid()
-	{
-		if(dataProvider.getDataDisplays().size()!=0)return;
-		dataProvider.addDataDisplay(dataGrid);
-		
-		request_card.findAllCards().with("user").fire(new Receiver<List<CardProxy>>() {
-			public void onSuccess(List <CardProxy> cards) {
-				//dataProvider.setList(cards);
-				dataProvider.getList().clear();
-				dataProvider.getList().addAll(cards);
-				cardLoadLabel.setHTML("<p>Status: Card Data loaded</p>");
-			}
-			@Override
-			public void onFailure(ServerFailure error) {
-				cardLoadLabel.setHTML("<p>"+error.getMessage()+"</p>");
-			}
-		});
-	}
-	
-	private void setUserList(List<UserProxy> users)
-	{
-		userlist.clear();
-		userlist.addAll(users);
-	}
-	
-	private String getUserDisplayName(UserProxy user)
-	{
-  	  if(user!=null) return user.getId() + " - " + user.getFirstname().substring(0, 15) + " " + user.getLastname().substring(0, 15);
-  	  else return "";
 	}
 }
---
+++ src/main/java/de/rtcustomz/getraenkeautomat/client/AdminUserPage.java
@@ -0,0 +1,288 @@
+package de.rtcustomz.getraenkeautomat.client;
+
+import java.util.Comparator;
+import java.util.List;
+
+import com.google.gwt.cell.client.EditTextCell;
+import com.google.gwt.cell.client.FieldUpdater;
+import com.google.gwt.cell.client.TextCell;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.ColumnSortEvent.ListHandler;
+import com.google.gwt.user.cellview.client.DataGrid;
+import com.google.gwt.user.cellview.client.SimplePager;
+import com.google.gwt.user.cellview.client.SimplePager.TextLocation;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.view.client.DefaultSelectionEventManager;
+import com.google.gwt.view.client.ListDataProvider;
+import com.google.gwt.view.client.MultiSelectionModel;
+import com.google.gwt.view.client.SelectionModel;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.SimpleEventBus;
+import com.google.web.bindery.requestfactory.shared.Receiver;
+import com.google.web.bindery.requestfactory.shared.ServerFailure;
+
+import de.rtcustomz.getraenkeautomat.client.proxies.UserProxy;
+import de.rtcustomz.getraenkeautomat.shared.ModelRequestFactory;
+import de.rtcustomz.getraenkeautomat.shared.requests.UserRequest;
+
+public class AdminUserPage extends Composite {
+	
+	FlowPanel page = new FlowPanel();
+	static private AdminUserPage _instance = null;
+	private static final String pageName = "Benutzer";
+	
+	public AdminUserPage() 
+	{
+        initPage();
+        initWidget(page);
+	}
+	
+    public static AdminUserPage getInstance(){
+        if(null == _instance) {
+            _instance = new AdminUserPage();
+        }
+        return _instance;
+    }
+    
+    public static String getPageName()
+    {
+    	return pageName;
+    }
+
+	//private final Messages messages = GWT.create(Messages.class);
+
+	private final ModelRequestFactory requestFactory = GWT.create(ModelRequestFactory.class);
+	
+	//-----\/-----
+	DataGrid<UserProxy> dataGrid;
+	SimplePager pager;
+	
+    private final ListDataProvider<UserProxy> dataProvider = new ListDataProvider<UserProxy>();
+    
+	UserRequest request_user = requestFactory.userRequest();
+	UserProxy newUserProxy = request_user.create(UserProxy.class);
+	
+	final EventBus eventBus = new SimpleEventBus();
+	
+	final HTML userLoadLabel = new HTML();
+	final HTML debugLabel = new HTML();
+	//-----/\-----
+	
+	/**
+	 * This is the entry point method.
+	 */
+	public void initPage() {
+    	
+    	//-----\/-----
+    	requestFactory.initialize(eventBus);
+		dataGrid = new DataGrid<UserProxy>();//(KEY_PROVIDER);
+		dataGrid.setWidth("100%");
+		dataGrid.setHeight("300px");
+		
+		//dataGrid.setAutoHeaderRefreshDisabled(true);
+		
+		dataGrid.setEmptyTableWidget(new Label("Leere Tabelle..."));
+		final ListHandler<UserProxy> sortHandler = new ListHandler<UserProxy>(dataProvider.getList());
+		dataGrid.addColumnSortHandler(sortHandler);
+		
+		SimplePager.Resources pagerResources = GWT.create(SimplePager.Resources.class);
+		pager = new SimplePager(TextLocation.CENTER, pagerResources, false, 0, true);
+		pager.setDisplay(dataGrid);
+		
+		final SelectionModel<UserProxy> selectionModel = new MultiSelectionModel<UserProxy>();
+		dataGrid.setSelectionModel(selectionModel, DefaultSelectionEventManager.<UserProxy> createCheckboxManager());
+		
+		/*request_user.findAllUsers().fire(new Receiver<List<UserProxy>>() {
+			@Override
+			public void onSuccess(List<UserProxy> users) {
+				dataProvider.getList().clear();
+				dataProvider.getList().addAll(users);
+				userLoadLabel.setHTML("<p>Status: User Data loaded</p>");
+			}
+			@Override
+			public void onFailure(ServerFailure error) {
+				userLoadLabel.setHTML("<p>"+error.getMessage()+"</p>");
+			}
+		});*/
+
+		initTableColumns(selectionModel, sortHandler);
+		
+		showGrid();
+		
+		page.add(userLoadLabel);
+		page.add(debugLabel);
+		page.add(dataGrid);
+		page.add(pager);
+		
+    	//-----/\-----
+	}
+	
+	/**
+	 * Add the columns to the table.
+	 */
+	private void initTableColumns(final SelectionModel<UserProxy> selectionModel,
+	    ListHandler<UserProxy> sortHandler) {
+//	  // Checkbox column. This table will uses a checkbox column for selection.
+//	  // Alternatively, you can call dataGrid.setSelectionEnabled(true) to enable
+//	  // mouse selection.
+//	  Column<UserProxy, Boolean> checkColumn =
+//	      new Column<UserProxy, Boolean>(new CheckboxCell(true, false)) {
+//	        @Override
+//	        public Boolean getValue(UserProxy object) {
+//	          // Get the value from the selection model.
+//	          return selectionModel.isSelected(object);
+//	        }
+//	      };
+//	  dataGrid.addColumn(checkColumn, SafeHtmlUtils.fromSafeConstant("<br/>"));
+//	  dataGrid.setColumnWidth(checkColumn, 40, Unit.PX);
+//		final List<UserProxy> card_list = new ArrayList<UserProxy>();
+		
+	  // id.
+	  Column<UserProxy, String> idColumn =
+	      new Column<UserProxy, String>(new TextCell()) {
+	        @Override
+	        public String getValue(UserProxy object) {
+	          return object.getId()+"";
+	        }
+	      };
+	  idColumn.setSortable(true);
+	  sortHandler.setComparator(idColumn, new Comparator<UserProxy>() {
+	    @Override
+	    public int compare(UserProxy o1, UserProxy o2) {
+	      return Integer.compare(o1.getId(), o2.getId());
+	    }
+	  });
+	  dataGrid.addColumn(idColumn, "ID");
+	  //dataGrid.setColumnWidth(idColumn, 140, Unit.PX);
+	  dataGrid.setColumnWidth(idColumn, 17, Unit.PCT);
+		
+	  // firstname.
+	  Column<UserProxy, String> firstNameColumn =
+	      new Column<UserProxy, String>(new EditTextCell()) {
+	        @Override
+	        public String getValue(UserProxy object) {
+	          return object.getFirstname();
+	        }
+	      };
+	      firstNameColumn.setSortable(true);
+	  sortHandler.setComparator(firstNameColumn, new Comparator<UserProxy>() {
+	    @Override
+	    public int compare(UserProxy o1, UserProxy o2) {
+	      return o1.getFirstname().compareTo(o2.getFirstname());
+	    }
+	  });
+	  dataGrid.addColumn(firstNameColumn, "Vorname");
+	  firstNameColumn.setFieldUpdater(new FieldUpdater<UserProxy, String>() {
+	    @Override
+	    public void update(int index, UserProxy object, String value) {
+	      // Called when the user changes the value.
+	    	request_user = requestFactory.userRequest();
+	    	UserProxy user = request_user.edit(object);
+	    	user.setFirstname(value);
+	    	request_user.save(user).fire(new Receiver<Void>(){
+				@Override
+				public void onSuccess(Void arg0) {
+					dataProvider.refresh();
+				}
+	    		
+	    	});
+	    }
+	  });
+	  //dataGrid.setColumnWidth(firstNameColumn, 330, Unit.PX);
+	  dataGrid.setColumnWidth(firstNameColumn, 33, Unit.PCT);
+	  
+	  // lastname.
+	  Column<UserProxy, String> lastNameColumn =
+	      new Column<UserProxy, String>(new EditTextCell()) {
+	        @Override
+	        public String getValue(UserProxy object) {
+	          return object.getLastname();
+	        }
+	      };
+	      lastNameColumn.setSortable(true);
+	  sortHandler.setComparator(lastNameColumn, new Comparator<UserProxy>() {
+	    @Override
+	    public int compare(UserProxy o1, UserProxy o2) {
+	      return o1.getLastname().compareTo(o2.getLastname());
+	    }
+	  });
+	  dataGrid.addColumn(lastNameColumn, "Nachname");
+	  lastNameColumn.setFieldUpdater(new FieldUpdater<UserProxy, String>() {
+	    @Override
+	    public void update(int index, UserProxy object, String value) {
+	      // Called when the user changes the value.
+	    	request_user = requestFactory.userRequest();
+	    	UserProxy user = request_user.edit(object);
+	    	user.setLastname(value);
+	    	request_user.save(user).fire(new Receiver<Void>(){
+				@Override
+				public void onSuccess(Void arg0) {
+					dataProvider.refresh();
+				}
+	    		
+	    	});
+	    }
+	  });
+	  //dataGrid.setColumnWidth(descriptionColumn, 330, Unit.PX);
+	  dataGrid.setColumnWidth(lastNameColumn, 33, Unit.PCT);
+	  
+	  // nickname.
+	  Column<UserProxy, String> nickNameColumn =
+	      new Column<UserProxy, String>(new EditTextCell()) {
+	        @Override
+	        public String getValue(UserProxy object) {
+	          return object.getNickname();
+	        }
+	      };
+	      nickNameColumn.setSortable(true);
+	  sortHandler.setComparator(nickNameColumn, new Comparator<UserProxy>() {
+	    @Override
+	    public int compare(UserProxy o1, UserProxy o2) {
+	      return o1.getNickname().compareTo(o2.getNickname());
+	    }
+	  });
+	  dataGrid.addColumn(nickNameColumn, "Spitzname");
+	  nickNameColumn.setFieldUpdater(new FieldUpdater<UserProxy, String>() {
+	    @Override
+	    public void update(int index, UserProxy object, String value) {
+	      // Called when the user changes the value.
+	    	request_user = requestFactory.userRequest();
+	    	UserProxy user = request_user.edit(object);
+	    	user.setNickname(value);
+	    	request_user.save(user).fire(new Receiver<Void>(){
+				@Override
+				public void onSuccess(Void arg0) {
+					dataProvider.refresh();
+				}
+	    		
+	    	});
+	    }
+	  });
+	  //dataGrid.setColumnWidth(descriptionColumn, 330, Unit.PX);
+	  dataGrid.setColumnWidth(nickNameColumn, 33, Unit.PCT);	  
+	}
+	
+	private void showGrid()
+	{
+		if(dataProvider.getDataDisplays().size()!=0)return;
+		dataProvider.addDataDisplay(dataGrid);
+		
+		request_user.findAllUsers().with("user").fire(new Receiver<List<UserProxy>>() {
+			public void onSuccess(List <UserProxy> cards) {
+				//dataProvider.setList(cards);
+				dataProvider.getList().clear();
+				dataProvider.getList().addAll(cards);
+				userLoadLabel.setHTML("<p>Status: Card Data loaded</p>");
+			}
+			@Override
+			public void onFailure(ServerFailure error) {
+				userLoadLabel.setHTML("<p>"+error.getMessage()+"</p>");
+			}
+		});
+	}
+}
